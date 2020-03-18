4fd6c96153bc24924357a7323ac826f60d0fb905
Andre <stolpakov2000@gmail.com>
2020-03-18 10:00:37 +0200

    right version files

--- Gis/views.py
+++ Gis/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    # return HttpResponse("Hello world")
+    return render(request, "Gis/home.html")
+
+
+
