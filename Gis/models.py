5b1523c8474ba9eaea44057946e5bd4688c625ca
Andre <stolpakov2000@gmail.com>
2020-03-24 23:55:42 +0200

    a few little changes

--- Gis/models.py
+++ Gis/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
+#from django import models
 # Create your models here.
 
-class Articles(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return  self.title
 
 
